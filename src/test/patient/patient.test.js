16fda858f3d29c3738d8fabb7dbe5b1a5e558038
mohammed <ibrahimdamy7@gmail.com>
2020-03-26 17:03:44 +0100

    set up test environment

---
+++ src/test/patient/patient.test.js
@@ -0,0 +1,15 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import { expect } from 'chai'
+
+// import app
+import app from '../../app';
+
+chai.use(chaiHttp);
+chai.should();
+
+describe('Patient Sign up', () => {
+    expect(nm).to.equal(4)
+
+
+})
